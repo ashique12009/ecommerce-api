e471ee02a028ac24ff892190bce53630127ca867
Ashique <ashique12009@gmail.com>
2026-04-03 19:55:31 +0300

    util jwt config added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the E-commerce API");
+});
+
 // app.use("/api/auth", require("./routes/auth.routes"));
 
 module.exports = app;
